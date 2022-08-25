4e611c5beb411f0e8073f74840bbb78ecdeab77c
mnt3710 <tomato3710@gmail.com>
2022-08-26 04:28:10 +0900

    フォーマッター+google fonts追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,26 @@ module.exports = {
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
- ],
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: theme => ({
+      backgroundImage: (theme) => ({
         'index-bg': "url('../../assets/bg.png')",
-      })
+      }),
+    },
+    fontFamily: {
+      'qwitcher-grypen': ['Qwitcher Grypen'],
+      silkscreen: ['silkscreen'],
+      'rubik-iso': ['Rubik Iso'],
+      caveat: ['Caveat'],
+      'amatic-sc': ['Amatic SC'],
+      'homemade-apple': ['Homemade Apple'],
+      megrim: ['Megrim'],
+      'permanent-marker': ['permanent Marker'],
+      'poiret-one': ['Poiret One'],
+      'alegreya-sans-cs': ['Alegreya Sans SC'],
+      'reenie-baenie': ['Reenie Beanie'],
     },
   },
   variants: {
